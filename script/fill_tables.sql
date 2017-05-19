bafd56b605365ce6fb78050c9b6ed9b5011edaa3
cristianominato <cminato35@gmail.com>
2017-05-18 21:56:27 -0300

    Update fill_tables.sql

--- script/fill_tables.sql
+++ script/fill_tables.sql
@@ -1,8 +1,6 @@
-
 insert into cadastroenviodesenho values ('nucleo', 'nucleo');
 insert into cadastroenviodesenho values ('bobina bt', 'bobinagem');
 insert into cadastroenviodesenho values ('calco', 'marcenaria');
-
 
 insert into cadastrodesenho values ('ct-3413', 0, 'calco');
 insert into cadastrodesenho values ('mp-12432', 0, 'nucleo');
