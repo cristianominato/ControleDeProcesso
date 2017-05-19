c9a749fcb5bbbb60ea14aee64ac5850968a2c074
cristianominato <cminato35@gmail.com>
2017-05-18 21:46:30 -0300

    Update create_tables.sql

--- script/create_tables.sql
+++ script/create_tables.sql
@@ -1,14 +1,18 @@
-CREATE TABLE usuario
-(
-  login character(20) NOT NULL,
-  senha character(20),
-  permissao character(20),
-  CONSTRAINT "PK_login" PRIMARY KEY (login)
-);
-
 CREATE TABLE cadastroenviodesenho
 (
   nome character(50) NOT NULL,
   enviar character(20),
   CONSTRAINT "PK_nome" PRIMARY KEY (nome)
+);
+
+CREATE TABLE cadastrodesenho
+(
+  desenho character(30) NOT NULL,
+  revisao integer,
+  cadastroenviodesenho character(50),
+  "FK_cadastroenviodesenho" character(50),
+  CONSTRAINT "PK_desenho" PRIMARY KEY (desenho),
+  CONSTRAINT "FK_cadastroenviodesenho" FOREIGN KEY ("FK_cadastroenviodesenho")
+      REFERENCES cadastroenviodesenho (nome) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
 );
