6b056806faf021b57e4c4b81cfc1a27497385ee7
cristianominato <cminato35@gmail.com>
2017-06-22 20:02:39 -0300

    Update create_tables.sql

--- script/create_tables.sql
+++ script/create_tables.sql
@@ -2,17 +2,45 @@ CREATE TABLE cadastroenviodesenho
 (
   nome character(50) NOT NULL,
   enviar character(20),
-  CONSTRAINT "PK_nome" PRIMARY KEY (nome)
+  projeto boolean,
+  nucleo boolean,
+  bobinagem boolean,
+  caldeiraria boolean,
+  parteativa boolean,
+  fechamento boolean,
+  pintura boolean,
+  laboratorio boolean,
+  almoxarifado boolean,
+  marcenaria boolean,
+  fkcadastrodesenho character(30),
+  CONSTRAINT nome PRIMARY KEY (nome),
+  CONSTRAINT fkcadastrodesenho FOREIGN KEY (fkcadastrodesenho)
+      REFERENCES cadastrodesenho (desenho) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
 );
+ALTER TABLE cadastroenviodesenho
+  OWNER TO postgres;
 
 CREATE TABLE cadastrodesenho
 (
   desenho character(30) NOT NULL,
-  revisao integer,
   cadastroenviodesenho character(50),
-  "FK_cadastroenviodesenho" character(50),
-  CONSTRAINT "PK_desenho" PRIMARY KEY (desenho),
-  CONSTRAINT "FK_cadastroenviodesenho" FOREIGN KEY ("FK_cadastroenviodesenho")
+  fkcadastroenviodesenho character(50),
+  fkcadastroestrutura character(10),
+  revisao character(10),
+  CONSTRAINT desenho PRIMARY KEY (desenho),
+  CONSTRAINT fkcadastroenviodesenho FOREIGN KEY (fkcadastroenviodesenho)
       REFERENCES cadastroenviodesenho (nome) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fkcadastroestrutura FOREIGN KEY (fkcadastroestrutura)
+      REFERENCES cadastroestrutura (estrutura) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
 );
+ALTER TABLE cadastrodesenho
+  OWNER TO postgres;
